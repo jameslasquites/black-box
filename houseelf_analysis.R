6fc90763f0db97aa4857156672454b45a02f4933
jameslasquites <jamesjadelasquites@gmail.com>
2016-10-07 11:16:37 -0500

    GC.percent for the dna sequence

--- houseelf_analysis.R
+++ houseelf_analysis.R
@@ -1,2 +1,14 @@
-houseelf <- read.csv("C:/Users/james/Desktop/Rstudio project/ASSIGNmenT_8/data/houseelf_earlength_dna_data.csv", header = TRUE)
+houseelf <- read.csv("C:/Users/james/Desktop/Rstudio project/ASSIGNmenT_8/data/houseelf_earlength_dna_data_1.csv", header = TRUE)
 houseelf
+library(stringr)
+
+GC.percent <- function(sequence){
+  Gs <- str_count(sequence, "G")
+  Cs <- str_count(sequence, "C")
+  GC <- round((Gs + Cs) / str_length(sequence) * 100, digits = 2)
+  return(GC)
+}
+
+for(sequence in houseelf$dnaseq){
+  print(round(GC.percent(sequence), digits = 2))
+}
